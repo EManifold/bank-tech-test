48a25e2920a135761edf36cfbcd34308ebb53be1
Ellie <elliemmanifold@gmail.com>
2020-01-27 13:52:21 +0000

    feat: withdraw method will minus the amount given from balance

--- spec/unit/bank_spec.rb
+++ spec/unit/bank_spec.rb
@@ -8,4 +8,12 @@ describe Bank do
       expect(subject.balance).to eq 500
     end
   end
+
+  describe '#withdraw' do
+    it "should decrease the balance by 300 when you deposit 300" do
+      subject.deposit(500)
+      subject.withdraw(300)
+      expect(subject.balance).to eq 200
+    end
+  end
 end
