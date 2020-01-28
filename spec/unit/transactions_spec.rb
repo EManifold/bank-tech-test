35f893d015e360ee44b9fe24f84235ded7e32a62
Ellie <elliemmanifold@gmail.com>
2020-01-28 17:48:20 +0000

    test: change transactions test to test behaviour rather than state, tests the output of the method I'm testing instead of the impact on a variable

--- spec/unit/transactions_spec.rb
+++ spec/unit/transactions_spec.rb
@@ -6,16 +6,14 @@ describe Transactions do
   describe '#deposit' do
     it "should add the transaction to the log if one deposit is made" do
       Timecop.freeze do
-        subject.entry(500, 900, :credit)
-        expect(subject.log).to eq [[Time.now.strftime("%d/%m/%Y"), "500.00", '', "900.00"]]
+        expect(subject.entry(500, 900, :credit)).to eq [[Time.now.strftime("%d/%m/%Y"), "500.00", '', "900.00"]]
       end
     end
 
     it "should add the transaction to the log if multiple deposits and transactions are made" do
       Timecop.freeze do
         subject.entry(500, 900, :credit)
-        subject.entry(300, 600, :debit)
-        expect(subject.log).to eq [[Time.now.strftime("%d/%m/%Y"), "500.00", '', "900.00"],
+        expect(subject.entry(300, 600, :debit)).to eq [[Time.now.strftime("%d/%m/%Y"), "500.00", '', "900.00"],
                                   [Time.now.strftime("%d/%m/%Y"), '', "300.00", "600.00"]]
       end
     end
