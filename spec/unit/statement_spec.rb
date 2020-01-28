c9206899d5d21ee0b75fb3dc5743fb9742abc678
Ellie <elliemmanifold@gmail.com>
2020-01-28 12:44:52 +0000

    chore: change variable name in tests from time to date

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -7,14 +7,14 @@ describe Statement do
   describe '#print_all' do
     it "should take in a transaction log and print it" do
       Timecop.freeze do
-        time = Time.now.strftime("%d/%m/%Y")
-        allow(transaction).to receive(:log) { [[time, "500.00", '', "500.00"],
-                                             [time, "300.00", '', "800.00"]]
+        date = Time.now.strftime("%d/%m/%Y")
+        allow(transaction).to receive(:log) { [[date, "500.00", '', "500.00"],
+                                             [date, "300.00", '', "800.00"]]
                               }
         statement = Statement.new(transaction)
         expect(statement.print_all).to eq "date || credit || debit || balance/n"\
-        "#{time} || 300.00 || || 800.00/n"\
-        "#{time} || 500.00 || || 500.00"
+        "#{date} || 300.00 || || 800.00/n"\
+        "#{date} || 500.00 || || 500.00"
       end
     end
   end
