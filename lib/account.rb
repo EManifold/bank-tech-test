4ec7bf5fdb4bb4242c931a46518cd3d18e3bca47
Ellie <elliemmanifold@gmail.com>
2020-01-28 14:04:51 +0000

    refactor: refactor account errors to separate hash

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,18 @@
 class Account
   attr_reader :balance
 
+  ERROR = {
+    valid_number: 'Please enter a valid number amount',
+    not_enough_funds: 'You do not have enough funds to withdraw that amount'
+  }
+
   def initialize(balance = 0, transactions = Transactions.new)
     @balance = balance
     @transactions = transactions
   end
 
   def deposit(amount)
-    raise 'Please enter a valid number amount' unless valid?(amount)
+    raise ERROR[:valid_number] unless valid?(amount)
 
     @balance += amount
     @transactions.entry(amount, @balance, :credit)
@@ -15,8 +20,8 @@ class Account
   end
 
   def withdraw(amount)
-    raise 'Please enter a valid number amount' unless valid?(amount)
-    raise 'You do not have enough funds to withdraw that amount' unless @balance >= amount
+    raise ERROR[:valid_number] unless valid?(amount)
+    raise ERROR[:not_enough_funds] unless @balance >= amount
 
     @balance -= amount
     @transactions.entry(amount, @balance, :debit)
