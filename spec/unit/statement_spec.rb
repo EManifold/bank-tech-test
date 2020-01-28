1d8ba41e714f31d0a5e710f33c403458a632256f
Ellie <elliemmanifold@gmail.com>
2020-01-28 11:24:31 +0000

    refactor: statement_spec line length and alignment to resolve rubocop errors

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -8,9 +8,12 @@ describe Statement do
     it "should take in a transaction log and print it" do
       Timecop.freeze do
         allow(transaction).to receive(:log) { [[Time.now.to_s[0...10], "500.00", '', "500.00"],
-                                             [Time.now.to_s[0...10], "300.00", '', "800.00"]] }
+                                             [Time.now.to_s[0...10], "300.00", '', "800.00"]]
+                              }                              
         statement = Statement.new(transaction)
-        expect(statement.print_all).to eq "date || credit || debit || balance/n#{Time.now.to_s[0...10]} || 300.00 || || 800.00/n#{Time.now.to_s[0...10]} || 500.00 || || 500.00"
+        expect(statement.print_all).to eq "date || credit || debit || balance/n"\
+        "#{Time.now.to_s[0...10]} || 300.00 || || 800.00/n"\
+        "#{Time.now.to_s[0...10]} || 500.00 || || 500.00"
       end
     end
   end
